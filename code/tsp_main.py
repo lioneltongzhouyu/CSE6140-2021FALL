96936e0af76811f78e6c6c1f43c92759ab59a37a
elvinxu <a961672623@qq.com>
2021-11-17 15:32:48 -0500

    fix: CLA

--- code/tsp_main.py
+++ code/tsp_main.py
@@ -3,23 +3,17 @@ from local_search_2 import LocalSearch2OPT
 from tsp import TSP
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"",["inst=", "alg=", "time=", "seed="])
-    except getopt.GetoptError:
-        print('tsp_main.py --inst <filename> --alg [BnB|Approx|LS1|LS2] --time <cutoff_in_seconds> [--seed <random_seed>]')
-        sys.exit(2)
-    
-    # print(opts)
+
     file_name, alg, time, seed = None, None, None, 0
-    for opt, arg in opts:
-        if opt == '--inst':
-            file_name = arg
-        elif opt == '--alg':
-            alg = arg
-        elif opt == '--time':
-            time = int(arg)
-        elif opt == '--seed':
-            seed = int(arg)
+    for i, arg in enumerate(argv):
+        if arg == '-inst' and i + 1 < len(argv):
+            file_name = argv[i + 1]
+        elif arg == '-alg' and i + 1 < len(argv):
+            alg = argv[i + 1]
+        elif arg == '-time' and i + 1 < len(argv):
+            time = int(argv[i + 1])
+        elif arg == '-seed' and i + 1 < len(argv):
+            seed = int(argv[i + 1])
             
     if file_name and alg and time >= 0:
         print("inst: ", file_name, " alg: ", alg, " time: ", time, " seed: ", seed)
@@ -40,7 +34,7 @@ def main(argv):
         
     else:
         print('Invalid input')
-        print('tsp_main.py --inst <filename> --alg [BnB|Approx|LS1|LS2] --time <cutoff_in_seconds> [--seed <random_seed>]')
+        print('tsp_main.py -inst <filename> -alg [BnB|Approx|LS1|LS2] -time <cutoff_in_seconds> [-seed <random_seed>]')
     
 
 if __name__ == "__main__":
